d8b6fb85afd5d2592134219fe82bec4598db51a6
edgar <edgar.riba@gmail.com>
2024-03-23 18:50:05 +0100

    improve benchmark

--- py-kornia/benchmark/bench_data.py
+++ py-kornia/benchmark/bench_data.py
@@ -8,28 +8,52 @@ import torch.utils.data as data
 import kornia_rs as KR
 import kornia as K
 import cv2
+import albumentations as A
 
 import time
 import timeit
 
 
+@torch.inference_mode()
 def kornia_transforms(img_path: str):
     img = KR.read_image_jpeg(img_path)
-    img = KR.resize(img, (32, 32), interpolation="bilinear")
-    return img
+    # img = KR.resize(img, (32, 32), interpolation="bilinear")
+    img = K.utils.image_to_tensor(img)
+    return img.float() / 255.0
 
 
 def opencv_transforms(img_path: str):
     img = cv2.imread(img_path)
-    img = cv2.resize(img, (32, 32), interpolation=cv2.INTER_LINEAR)
+    # img = cv2.resize(img, (32, 32), interpolation=cv2.INTER_LINEAR)
     return img
 
 
+class Augmentator:
+    def __init__(self, backend: str) -> None:
+        self.backend = backend
+
+        if "kornia" in backend:
+            self.augmentation = K.augmentation.ColorJitter(0.1, 0.1, 0.1, 0.1)
+        elif backend == "opencv":
+            self.augmentation = A.ColorJitter(
+                brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1
+            )
+
+    def __call__(self, img):
+        if self.backend == "kornia_rs":
+            img = self.augmentation(img)
+        elif self.backend == "opencv":
+            img = self.augmentation(image=img)["image"]
+            img = K.utils.image_to_tensor(img).float() / 255.0
+        return img
+
+
 class ImageDataset(data.Dataset):
-    def __init__(self, root: Path, backend: str) -> None:
+    def __init__(self, root: Path, backend: str, augment) -> None:
         self.root = root
         self.backend = backend
         self.images = list(self.root.glob("*.jpeg"))
+        self.augment = augment
 
     def __len__(self):
         return len(self.images)
@@ -38,35 +62,32 @@ class ImageDataset(data.Dataset):
         img_path = str(self.images[idx])
         if self.backend == "kornia_rs":
             img = kornia_transforms(img_path)
+            img = self.augment(img)
         elif self.backend == "opencv":
             img = opencv_transforms(img_path)
+            img = self.augment(img)
         elif self.backend in ["kornia_cpu", "kornia_gpu"]:
-            img = K.io.load_image(img_path, K.io.ImageLoadType.RGB8)
+            img = K.io.load_image(img_path, K.io.ImageLoadType.RGB32)
 
         return img
 
 
-@torch.compile
-def jresize(img: torch.Tensor):
-    return K.geometry.resize(img, (32, 32), interpolation="bilinear")
-
-
 def load_dataset_pipeline(images_dir: str, backend: str, num_workers: int):
 
-    dataset = ImageDataset(Path(images_dir), backend=backend)
+    augment = Augmentator(backend)
+
+    dataset = ImageDataset(Path(images_dir), backend=backend, augment=augment)
     dataloader = data.DataLoader(
         dataset, batch_size=64, shuffle=False, num_workers=num_workers
     )
 
     for sample in dataloader:
         with torch.inference_mode():
-            if backend == "kornia_cpu":
-                # img = K.geometry.resize(sample, (32, 32), interpolation="bilinear")
-                img = jresize(sample)
-            elif backend == "kornia_gpu":
-                img = K.geometry.resize(
-                    sample.cuda(), (32, 32), interpolation="bilinear"
-                )
+            if backend == "kornia_cpu" or backend == "kornia_gpu":
+                if "gpu" in backend:
+                    sample = sample.cuda()
+
+                img = augment(sample)
 
 
 def main():
@@ -90,6 +111,12 @@ def main():
         choices=["kornia_cpu", "kornia_gpu", "kornia_rs", "opencv"],
         help="backend to use for the benchmark",
     )
+    parser.add_argument(
+        "--num-iterations",
+        type=int,
+        default=10,
+        help="number of iterations to run the benchmark",
+    )
     args = parser.parse_args()
 
     timer = timeit.Timer(
@@ -109,7 +136,9 @@ def main():
     print(f"Batch size: {args.batch_size}")
     print(f"Number of workers: {args.num_workers}")
     print(f"Backend: {args.backend}")
-    print(f"Time: {timer.timeit(10) * 1000:.2f} ms")
+    print(f"Number of iterations: {args.num_iterations}")
+
+    print(f"Time: {timer.timeit(args.num_iterations) * 1000:.2f} ms")
 
 
 if __name__ == "__main__":
